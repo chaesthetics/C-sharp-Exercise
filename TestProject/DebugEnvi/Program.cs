92be976ec0eba2c93f1d2d82002c804f72d0ae9d
Auriel James <aurieljames11@gmail.com>
2023-11-02 11:48:20 +0800

    CreateAndThrow-Exceptions

--- TestProject/DebugEnvi/Program.cs
+++ TestProject/DebugEnvi/Program.cs
@@ -1,38 +1,80 @@
 try
 {
-    int num1 = int.MaxValue;
-    int num2 = int.MaxValue;
-    int result = num1 + 2;
-    Console.WriteLine("Result: " + result);
-
-    string str = "Aurieljames";
-    int length = str.Length;
-    Console.WriteLine("String Length: " + length);
-
-    int[] numbers = new int[5];
-    numbers[5] = 10;
-    Console.WriteLine("Number at index 5: " + numbers[5]);
-
-    int num3 = 10;
-    int num4 = 0;
-    int result2 = num3 / num4;
-    Console.WriteLine("Result: " + result2);
+    OperatingProcedure1();
 }
-catch (OverflowException ex)
+catch (Exception ex)
 {
-    Console.WriteLine("Error: The number is too large to be represented as an integer." + ex.Message);
-}
-catch (NullReferenceException ex)
-{
-    Console.WriteLine("Error: " + ex.Message);
-}
-catch (IndexOutOfRangeException ex)
-{
-    Console.WriteLine("Error: Index out of range." + ex.Message);
-}
-catch (DivideByZeroException ex)
-{
-    Console.WriteLine("Error: Cannot divide by zero." + ex.Message);
+    Console.WriteLine(ex.Message);
+    Console.WriteLine("Exiting application.");
 }
 
-Console.WriteLine("Exiting program.");
+static void OperatingProcedure1()
+{
+    string[][] userEnteredValues = new string[][]
+    {
+        new string[] { "1", "two", "3"},
+        new string[] { "0", "1", "2"}
+    };
+
+    foreach(string[] userEntries in userEnteredValues)
+    {
+        try
+        {
+            BusinessProcess1(userEntries);
+        }
+        catch (Exception ex)
+        {
+            if (ex.StackTrace.Contains("BusinessProcess1"))
+            {
+                if (ex is FormatException)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Corrective action taken in OperatingProcedure1");
+                }
+                else if (ex is DivideByZeroException)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Partial correction in OperatingProcedure1 - further action required");
+
+                    // re-throw the original exception
+                    throw;
+                }
+                else
+                {
+                    // create a new exception object that wraps the original exception
+                    throw new ApplicationException("An error occurred - ", ex);
+                }
+            }
+        }
+
+    }
+}
+
+static void BusinessProcess1(String[] userEntries)
+{
+    int valueEntered;
+
+    foreach (string userValue in userEntries)
+    {
+        try
+        {
+            valueEntered = int.Parse(userValue);
+
+            checked
+            {
+                int calculatedValue = 4 / valueEntered;
+            }
+        }
+        catch (FormatException)
+        {
+            FormatException invalidFormatException = new FormatException("FormatException: User input values in 'BusinessProcess1' must be valid integers");
+            throw invalidFormatException;
+        }
+        catch (DivideByZeroException)
+        {
+            DivideByZeroException unexpectedDivideByZeroException = new DivideByZeroException("DivideByZeroException: Calculation in 'BusinessProcess1' encountered an unexpected divide by zero");
+            throw unexpectedDivideByZeroException;
+
+        }
+    }
+}
